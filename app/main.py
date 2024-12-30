45191eab4cab6f05606bd134a3396c076d8a9e6e
Jan Str <janstr06@gmail.com>
2024-12-30 21:58:38 +0100

    update

---
+++ app/main.py
@@ -0,0 +1,45 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Header
+from pydantic import BaseModel
+from .config import KUBERNETES_KEY, KUBERNETES_URL, PUBLIC_KEY_PATH
+from .models import Kubernetes, RedisConnector, Database
+from jwt import decode
+
+kube: Kubernetes
+
+@asynccontextmanager
+async def init(app: FastAPI):
+    global kube
+    kube = Kubernetes(key=KUBERNETES_KEY, url=KUBERNETES_URL)
+    yield
+
+app = FastAPI(lifespan=init)
+
+
+class Data(BaseModel):
+    # user_id: int
+    cmd: str
+
+@app.post("/exec")
+def execute_command(x_token: Annotated[str, Header()], data: Data):
+    token = None
+    try:
+        token = decode(x_token, key=open(PUBLIC_KEY_PATH).read(), algorithms=['RS256'])
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return {"result": kube.execute_command(user_id=token["sub"], command=data.cmd)}
+@app.post("/create")
+def create_accessbox(x_token: Annotated[str, Header()]):
+    token = None
+    try:
+        token = decode(x_token, key=open(PUBLIC_KEY_PATH).read(), algorithms=['RS256'])
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    kube.create_accessbox(user_id=token["sub"])
+    return {"status": "ok"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
